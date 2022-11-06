8def1696b28d0f32c58cbe36c6036e48ce58e9f1
grogu\dani <danieljerezgarrido@gmail.com>
2022-11-06 15:46:15 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using Serilog;
+using ShellProgressBar;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var filePath = AppDomain.CurrentDomain.BaseDirectory;
+
+        Log.Logger = new LoggerConfiguration()
+                     .Enrich.FromLogContext()
+                     .MinimumLevel.Debug()
+                     .WriteTo.File(filePath + @"\logs\Updater_.txt", rollingInterval: RollingInterval.Day)
+                     .CreateLogger();
+
+        if (args.Length < 3)
+            return;
+
+        var fileName = args[0];
+        var downloadFileUrl = args[1];
+        var exe = args[2];
+
+        IProgress<float>? progress = null;
+
+        var options = new ProgressBarOptions
+        {
+            ForegroundColor = ConsoleColor.Yellow,
+            ForegroundColorDone = ConsoleColor.DarkGreen,
+            BackgroundColor = ConsoleColor.DarkGray,
+            ForegroundColorError = ConsoleColor.Red,
+            BackgroundCharacter = '\u2593',
+            CollapseWhenFinished = false
+        };
+
+        using (var main = new ProgressBar(3, $"update process '{exe}'", options))
+        {
+            try
+            {
+                var step1 = await Steps.DownloadFile(progress, main, options, downloadFileUrl, fileName);
+                if (!step1) return;
+                var step2 = Steps.UnZipFile(progress, main, options, fileName, filePath);
+                if (!step2) return;
+                var step3 = Steps.OpenExe(progress, main, options, exe);
+            }
+            catch (Exception e)
+            {
+                main.WriteErrorLine(e.Message);
+                Log.Error(e.Message, e);
+            }
+
+        }
+
+        Environment.Exit(0);
+
+    }
+
+}
+
+
+
---
+++ Steps.cs
@@ -0,0 +1,100 @@
+﻿using Ionic.Zip;
+using Serilog;
+using ShellProgressBar;
+using System.Diagnostics;
+
+public static class Steps
+{
+    public static async Task<bool> DownloadFile(IProgress<float>? progress, ProgressBar main, ProgressBarOptions options, string downloadFileUrl, string fileName)
+    {
+        using var child = main.Spawn(int.MaxValue, "starting download", options);
+
+        try
+        {
+            using (var client = new HttpClientDownloadWithProgress(downloadFileUrl, fileName))
+            {
+                client.ProgressChanged += (totalFileSize, totalBytesDownloaded, progressPercentage) =>
+                {
+
+                    if (progressPercentage == null || totalFileSize == null)
+                        return;
+
+                    if (progress == null)
+                    {
+                        progress = child.AsProgress<float>();
+                        child.MaxTicks = (int)totalFileSize;
+                        child.Message = $"download '{fileName}' to '{downloadFileUrl}'";
+                    }
+
+                    progress.Report((float)progressPercentage / 100);
+                };
+
+                await client.StartDownload();
+            }
+            main.Tick();
+            return true;
+        }
+        catch (Exception e)
+        {
+            child.ForegroundColor = ConsoleColor.Red;
+            child.WriteErrorLine(e.Message);
+            Log.Error(e.Message, e);
+            return false;
+        }
+        
+    }
+
+    public static bool UnZipFile(IProgress<float> progress, ProgressBar main, ProgressBarOptions options, string fileName, string filePath)
+    {
+        using var child = main.Spawn(100, $"unzip '{fileName}' in '{filePath}'", options);
+
+        try
+        {
+
+            progress = child.AsProgress<float>();
+            progress.Report(100);
+
+            using (ZipFile archive = new ZipFile(filePath + fileName))
+            {
+                archive.RemoveSelectedEntries("updater.exe");
+                archive.ExtractAll(filePath, ExtractExistingFileAction.OverwriteSilently);
+            }
+
+            main.Tick();
+            return true;
+        }
+        catch (Exception e)
+        {
+            progress.Report(0);
+            child.ForegroundColor = ConsoleColor.Red;
+            child.WriteErrorLine(e.Message);
+            Log.Error(e.Message, e);
+            return false;
+        }
+
+        
+    }
+
+    public static bool OpenExe(IProgress<float> progress, ProgressBar main, ProgressBarOptions options, string exe)
+    {
+        using var child = main.Spawn(100, $"update finished. opening '{exe}'", options);
+        try
+        {
+            progress = child.AsProgress<float>();
+            progress.Report(100);
+            Process.Start(exe);
+
+            main.Tick();
+            return true;
+        }
+        catch (Exception e)
+        {
+            progress.Report(0);
+            child.ForegroundColor= ConsoleColor.Red;
+            child.WriteErrorLine(e.Message);
+            Log.Error(e.Message, e);
+            return false;
+        }
+        
+    }
+}
