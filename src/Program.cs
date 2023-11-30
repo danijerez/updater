ca8ef41cccb4841b104344975b836d65a26a0118
dani <danieljerezgarrido@gmail.com>
2023-11-30 09:42:37 +0100

    update dotnet 8

--- src/Program.cs
+++ src/Program.cs
@@ -8,27 +8,9 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        var version = Assembly.GetExecutingAssembly().GetName().Version;
-
-        Log.Logger = new LoggerConfiguration()
-                     .Enrich.FromLogContext()
-                     .MinimumLevel.Debug()
-                     .WriteTo.File(AppDomain.CurrentDomain.BaseDirectory + $@"\logs\updater_{version}_.txt", rollingInterval: RollingInterval.Day)
-                     .CreateLogger();
-
         var a = Read(args);
-
-        IProgress<float>? progress = null;
-
-        var options = new ProgressBarOptions
-        {
-            ForegroundColor = ConsoleColor.Yellow,
-            ForegroundColorDone = ConsoleColor.DarkGreen,
-            BackgroundColor = ConsoleColor.DarkGray,
-            ForegroundColorError = ConsoleColor.Red,
-            BackgroundCharacter = '\u2593',
-            CollapseWhenFinished = false
-        };
+        ConfigureLogger();
+        var options = ConfigureProgressBar();
 
         var nSteps = 2;
         if (a.OpenExe != null) nSteps++;
@@ -38,10 +20,10 @@ class Program
             try
             {
                 if (a.DownloadUrl == null || a.FileName == null || a.FilePath == null) return;
-                if (!await DownloadFile(progress, main, options, a)) return;
-                if (!UnpackDownload(progress, main, options, a)) return;
+                if (!await DownloadFile(null, main, options, a)) return;
+                if (!UnpackDownload(null, main, options, a)) return;
                 RemoveFilesOrDirectory(a);
-                if (a.OpenExe != null) OpenExe(progress, main, options, a);
+                if (a.OpenExe != null) OpenExe(null, main, options, a);
                 if (a.WaitClose) EndWait(main);
             }
             catch (Exception e)
@@ -49,12 +31,29 @@ class Program
                 main.WriteErrorLine(e.Message);
                 Log.Error(e.Message, e);
             }
-
         }
-
     }
 
+    private static void ConfigureLogger()
+    {
+        var version = Assembly.GetExecutingAssembly().GetName().Version;
+        Log.Logger = new LoggerConfiguration()
+                     .Enrich.FromLogContext()
+                     .MinimumLevel.Debug()
+                     .WriteTo.File(AppDomain.CurrentDomain.BaseDirectory + $@"\logs\updater_{version}_.txt", rollingInterval: RollingInterval.Day)
+                     .CreateLogger();
+    }
+
+    private static ProgressBarOptions ConfigureProgressBar()
+    {
+        return new ProgressBarOptions
+        {
+            ForegroundColor = ConsoleColor.Yellow,
+            ForegroundColorDone = ConsoleColor.DarkGreen,
+            BackgroundColor = ConsoleColor.DarkGray,
+            ForegroundColorError = ConsoleColor.Red,
+            BackgroundCharacter = '\u2593',
+            CollapseWhenFinished = false
+        };
+    }
 }
-
-
-
